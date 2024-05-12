f197045c17d7a59695e787bb777908bb161fedaf
dohmeid <doha.hmeid@gmail.com>
2024-05-12 12:28:43 +0300

    adding the who to follow section

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,11 +1,13 @@
 import React from "react";
 import classes from "./Body.module.css";
 import Account from "./Account/Account";
+import Follow from "./Follow/Follow";
 
 const Body = () => {
   return (
     <div className={classes.container}>
       <Account />
+      <Follow/>
     </div>
   );
 };
---
+++ src/components/Body/Follow/FollowAccount/FollowAccount.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classes from "./FollowAccount.module.css";
+import ProfileHeader from "../../../Shared/ProfileHeader/ProfileHeader";
+import FollowButton from "../../../Shared/FollowButton/FollowButton";
+
+const FollowAccount = (props) => {
+  return (
+    <div className={classes.container}>
+      <p className={classes.followers}>
+        <i class="bi bi-person-fill"></i>
+        {props.followers}
+      </p>
+
+      <div className={classes.accountHeader}>
+        <ProfileHeader
+          photo={props.photo}
+          name={props.name}
+          userName={props.userName}
+        />
+        <FollowButton />
+      </div>
+
+      <p className={classes.headline}>{props.text}</p>
+    </div>
+  );
+};
+
+export default FollowAccount;
